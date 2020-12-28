5254cfeeee3f65d81c254e8fb4456d86f97d6ee6
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-12-28 22:47:11 +0700

    View Product Detail

--- Web-ASP.NET-MVC/Controllers/ProductsController.cs
+++ Web-ASP.NET-MVC/Controllers/ProductsController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Web_ASP.NET_MVC.Models;
 using PagedList;
+using System.Net;
+
 namespace Web_ASP.NET_MVC.Controllers
 {
     public class ProductsController : Controller
@@ -20,6 +22,19 @@ namespace Web_ASP.NET_MVC.Controllers
             var pageSize = 6;
             var productList = db.Products.OrderByDescending(x => x.ProductCode).ToPagedList(pageNumber, pageSize);
             return PartialView(productList);
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product pro = db.Products.Find(id);
+            if (pro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pro);
         }
     }
 }
