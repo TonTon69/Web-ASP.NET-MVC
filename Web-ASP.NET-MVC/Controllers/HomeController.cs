ca54e16b1521369247d9f93b44661e2789509c35
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-12-28 19:40:35 +0700

    get products highlight of the home

--- Web-ASP.NET-MVC/Controllers/HomeController.cs
+++ Web-ASP.NET-MVC/Controllers/HomeController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_ASP.NET_MVC.Models;
 
 namespace Web_ASP.NET_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        ShopFashionContext db = new ShopFashionContext();
         public ActionResult Index()
         {
             return View();
         }
-
+        public PartialViewResult ProductPartial()
+        {
+            var productHighlight = db.Products.OrderByDescending(x => x.ProductCode).Take(5).ToList();
+            return PartialView(productHighlight);
+        }
     }
 }
