59971d883170ef0e2ac0e7f781ed0e3ecbf4f7e4
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2021-01-10 22:13:56 +0700

    get list order

---
+++ Web-ASP.NET-MVC/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_ASP.NET_MVC.Models;
+
+namespace Web_ASP.NET_MVC.Areas.Admin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        ShopFashionContext db = new ShopFashionContext();
+        // GET: Admin/Orders
+        public ActionResult Index()
+        {
+            var count = db.OrderDetails.Count();
+            ViewBag.message = count;
+            return View(db.OrderDetails.ToList());
+        }
+    }
+}
