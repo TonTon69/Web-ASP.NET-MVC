ee4c15e298461343188541b415b09d427ac46b47
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2021-01-15 14:38:19 +0700

    bind feedback admin page

---
+++ Web-ASP.NET-MVC/Areas/Admin/Controllers/FeedbacksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_ASP.NET_MVC.Models;
+
+namespace Web_ASP.NET_MVC.Areas.Admin.Controllers
+{
+    public class FeedbacksController : Controller
+    {
+        ShopFashionContext db = new ShopFashionContext();
+        // GET: Admin/Feedbacks
+        public ActionResult Index()
+        {
+            //dem so luong 
+            var count = db.FeedBacks.Count();
+            ViewBag.message = count;
+
+            return View(db.FeedBacks.ToList());
+        }
+    }
+}
